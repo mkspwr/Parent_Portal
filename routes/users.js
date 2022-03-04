14714830da94ce78dcedfc54f8ce91c3a0f582bf
Kanchan Sharma <58194538+Kakkoo@users.noreply.github.com>
2022-03-04 10:51:30 -0800

    getuser api added

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,13 @@ router.get('/', function(req, res, next) {
   console.log("first level users");
 });
 
+router.get("/getuser", (req, res) => {
+    console.log(req.body);
+    User.findOne({email: req.body.email}).then((user) => {
+        if(user){
+            return res.status(200).json({ user: user });
+        }
+    })
+  })
+
 module.exports = router;
